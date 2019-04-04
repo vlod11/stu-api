f3eb032abb3d9ef63cf017f15739afa033494dfe
vlod11 <11odinets@gmail.com>
2019-04-04 10:22:03 +0300

    [fix] posts

--- UniHub.WebApi/DataAccess/RepositoryService/Repositories/FacultyRepository.cs
+++ UniHub.WebApi/DataAccess/RepositoryService/Repositories/FacultyRepository.cs
@@ -18,7 +18,7 @@ namespace UniHub.WebApi.DataAccess.RepositoryService
             return await _dbContext.Faculties
                                     .Where(f => f.UniversityId == universityId)
                                     .Skip(skip).Take(take)
-                                    .OrderBy(f => f.FullTitle)
+                                    .OrderBy(f => f.Id)
                                     .ToListAsync();
         }
     }
