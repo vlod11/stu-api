c0420c791914e40266fb7afba50190ea5ca9c85c
vlod11 <11odinets@gmail.com>
2019-06-28 18:20:15 +0300

    add todo comments

--- UniHub.WebApi/ModelLayer/Requests/Authorization/RegisterUserRequest.cs
+++ UniHub.WebApi/ModelLayer/Requests/Authorization/RegisterUserRequest.cs
@@ -13,6 +13,7 @@ namespace UniHub.WebApi.ModelLayer.Requests
         [EmailAddress]
         public string Email { get; set; }
 
+        // TODO: return validation
         //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+=\[{\]};:<>|./?,-]).{6,32}$")]
         public string Password { get; set; }
     }
--- UniHub.WebApi/ModelLayer/Requests/UserProfile/UpdatePasswordRequest.cs
+++ UniHub.WebApi/ModelLayer/Requests/UserProfile/UpdatePasswordRequest.cs
@@ -4,6 +4,7 @@ namespace UniHub.WebApi.ModelLayer.Requests.User
 {
     public class UpdatePasswordRequest
     {
+        // TODO: return validation
         //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+=\[{\]};:<>|./?,-]).{6,32}|$")]
         public string NewPassword { get; set; }
         public string CurrentPassword { get; set; }
