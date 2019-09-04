e015bc74003c784a56bfb420b305b62b7b77bb74
vlod11 <11odinets@gmail.com>
2019-09-04 11:45:09 +0300

    [feature] filter on post

--- UniHub.WebApi/DataAccess/RepositoryService/Interfaces/IPostRepository.cs
+++ UniHub.WebApi/DataAccess/RepositoryService/Interfaces/IPostRepository.cs
@@ -4,12 +4,14 @@ using System.Linq.Expressions;
 using System;
 using UniHub.WebApi.ModelLayer.Entities;
 using UniHub.WebApi.ModelLayer.Models;
+using UniHub.WebApi.ModelLayer.Enums;
 
 namespace UniHub.WebApi.DataAccess.RepositoryService
 {
     public interface IPostRepository : IBaseRepository<Post> 
     {
-        Task<IEnumerable<PostByGroup>> GetAllPostsFullBySubjectAsync(int subjectId, int skip, int take);
+        Task<IEnumerable<PostByGroup>> GetAllPostsFullBySubjectAsync(int subjectId, int skip, int take,
+            string title = "", int groupId = 0, int? semester = 0, EPostValueType? valueType = null, EPostLocationType? locationType = null);
         Task<Post> GetFullPostInfoAsync(int postId);
         Task<IEnumerable<Post>> GetFullUsersPostAsync(int userId, int skip, int take);
     }
--- UniHub.WebApi/DataAccess/RepositoryService/Repositories/PostRepository.cs
+++ UniHub.WebApi/DataAccess/RepositoryService/Repositories/PostRepository.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using UniHub.WebApi.ModelLayer.Entities;
+using UniHub.WebApi.ModelLayer.Enums;
 using UniHub.WebApi.ModelLayer.Models;
 
 namespace UniHub.WebApi.DataAccess.RepositoryService
@@ -13,16 +14,43 @@ namespace UniHub.WebApi.DataAccess.RepositoryService
         {
         }
 
-        public async Task<IEnumerable<PostByGroup>> GetAllPostsFullBySubjectAsync(int subjectId, int skip, int take)
+        public async Task<IEnumerable<PostByGroup>> GetAllPostsFullBySubjectAsync(int subjectId, int skip, int take,
+            string title = "", int groupId = 0, int? semester = 0, EPostValueType? valueType = null, EPostLocationType? locationType = null)
         {
-            return await _dbContext.Posts
+            IQueryable<Post> posts = _dbContext.Posts
                                     .Where(p => p.SubjectId == subjectId)
                                     .Include(p => p.Group)
                                     .Include(p => p.Votes)
                                     .OrderByDescending(s => s.GroupId)
                                     .ThenBy(p => p.Semester)
-                                    .ThenBy(p => p.GivenAt)
-                                    .GroupBy(p => p.Group, p => p)
+                                    .ThenBy(p => p.GivenAt);
+
+            if (!string.IsNullOrEmpty(title))
+            {
+                posts = posts.Where(p => p.Title.Contains(title));
+            }
+
+            if (groupId != 0)
+            {
+                posts = posts.Where(p => p.GroupId == groupId);
+            }
+
+            if (semester != 0)
+            {
+                posts = posts.Where(p => p.Semester == semester);
+            }
+
+            if (valueType != null)
+            {
+                posts = posts.Where(p => p.PostValueTypeId == (int)valueType);
+            }
+
+            if (locationType != null)
+            {
+                posts = posts.Where(p => p.PostLocationTypeId == (int)locationType);
+            }
+
+            return await posts.GroupBy(p => p.Group, p => p)
                                     .Select(g => new PostByGroup
                                     {
                                         GroupId = g.Key.Id,
